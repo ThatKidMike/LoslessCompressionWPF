0e2f63910aac28dc982a881094e723f0200688c2
ThatKidMike <44540485+ThatKidMike@users.noreply.github.com>
2019-10-28 14:53:39 +0100

    28.10 (preparing the UI for Arithmetic Coding)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using Microsoft.Msagl.Drawing;
+using System.Collections.ObjectModel;
+using WPFInterop.ViewModels;
 
 namespace WPFInterop
 {
@@ -23,100 +25,28 @@ namespace WPFInterop
     public partial class MainWindow : Window
     {
 
-        private ManagedHuffmanObj obj;
-
         public MainWindow()
         {
             InitializeComponent();
-            gViewer.ToolBarIsVisible = false;
-            theButton.Click += TheButton_Click;
-            gViewer.KeyDown += new System.Windows.Forms.KeyEventHandler(_ViewerKeyDown);
-            KeyDown += HostWindowKeyDown;
-            KeyUp += HostWindowKeyUp;
-            gViewer.MouseClick += new System.Windows.Forms.MouseEventHandler(_ViewerMouseClick);
- 
-            System.Drawing.SolidBrush solidWhite = new System.Drawing.SolidBrush(System.Drawing.Color.White);
-            gViewer.OutsideAreaBrush = solidWhite;
-
         }
 
-        private void TheButton_Click(object sender, RoutedEventArgs e)
+        private void HuffmanView_Clicked(object sender, RoutedEventArgs e)
         {
-
-            if (inputBox.Text != "")
-            {
-                obj = new ManagedHuffmanObj(inputBox.Text);
-
-                gViewer.Graph = obj.HuffmanTreeTraverse(obj.GetApex());
-
-                obj.delete();
-
-            }
+            DataContext = new HuffmanViewModel();
+            inputBox.Clear();
+            outputBlock.Text = "";
         }
 
-        private void HostWindowKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        private void ArithmeticView_Clicked(object sender, RoutedEventArgs e)
         {
-            switch (e.Key)
-            {
-                case Key.LeftCtrl:
-                    _ViewerKeyDown(null, new System.Windows.Forms.KeyEventArgs(Keys.Control));
-                    break;
-            }
+            DataContext = new ArithmeticViewModel();
+            inputBox.Clear();
+            outputBlock.Text = "";
         }
 
-        private void HostWindowKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        private void Proceed_Clicked(object sender, RoutedEventArgs e)
         {
-            switch (e.Key)
-            {
-                case Key.LeftCtrl:
-                    _ViewerKeyUp(null, new System.Windows.Forms.KeyEventArgs(Keys.Control));
-                    break;
-            }
-
+          
         }
-
-        private void _ViewerKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
-        {
-            if (Keyboard.IsKeyDown(Key.LeftCtrl))
-            {     
-                    gViewer.PanButtonPressed = true;
-                    UpdateCursor(System.Windows.Forms.Cursors.Hand);
-            }
-        }
-
-        private void _ViewerKeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
-        { 
-
-            if (!Keyboard.IsKeyDown(Key.LeftCtrl)) 
-            { 
-                gViewer.PanButtonPressed = false;
-                UpdateCursor(System.Windows.Forms.Cursors.Default);
-            }
-        }
-
-        private void UpdateCursor(System.Windows.Forms.Cursor cursor)
-        {
-            gViewer.Cursor = cursor;
-        }
-
-        private void _ViewerMouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
-        {
-            try
-            {
-                if (gViewer.SelectedObject.GetType() == typeof(Node) && gViewer.SelectedObject != null)
-                {
-                    var selectedObj = gViewer.SelectedObject;
-                    Node selectedNode = (Node)selectedObj;
-                    string code = obj.GetCodedSigns()[selectedNode.LabelText];
-                    if (code != null) 
-                    outputBlock.Text = selectedNode.LabelText + " :: " + code;                
-                }
-            } catch(Exception ex)
-            {
-                ex.ToString();
-            }
-
-        }
-
     }
 }
---
+++ Views/HuffmanView.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Msagl.Drawing;
+using WPFInterop.ViewModels;
+
+namespace WPFInterop.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy HuffmanView.xaml
+    /// </summary>
+    public partial class HuffmanView : System.Windows.Controls.UserControl
+    {
+
+        private ManagedHuffmanObj huffmanObj;
+        private MainWindow parentWindow;
+
+        public HuffmanView()
+        {
+            InitializeComponent();
+
+            gViewer.ToolBarIsVisible = false;
+            gViewer.KeyDown += new System.Windows.Forms.KeyEventHandler(_ViewerKeyDown);
+            KeyDown += HostWindowKeyDown;
+            KeyUp += HostWindowKeyUp;
+            gViewer.MouseClick += new System.Windows.Forms.MouseEventHandler(_ViewerMouseClick);
+
+            System.Drawing.SolidBrush solidWhite = new System.Drawing.SolidBrush(System.Drawing.Color.White);
+            gViewer.OutsideAreaBrush = solidWhite;
+
+            Loaded += (s, e) =>
+            {
+                parentWindow = Window.GetWindow(this) as MainWindow;
+                if (parentWindow != null)
+                {
+                    parentWindow.proceedButton.Click += ProceedButtonClicked;
+                }
+            };
+        }
+
+        private void ProceedButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (parentWindow.DataContext.GetType() == typeof(HuffmanViewModel) && parentWindow.inputBox.Text != "")
+            {
+                huffmanObj = new ManagedHuffmanObj(parentWindow.inputBox.Text);
+                gViewer.Graph = huffmanObj.HuffmanTreeTraverse(huffmanObj.GetApex());
+                huffmanObj.delete();
+            }
+        }
+
+        private void HostWindowKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.LeftCtrl:
+                    _ViewerKeyDown(null, new System.Windows.Forms.KeyEventArgs(Keys.Control));
+                    break;
+            }
+        }
+
+        private void HostWindowKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.LeftCtrl:
+                    _ViewerKeyUp(null, new System.Windows.Forms.KeyEventArgs(Keys.Control));
+                    break;
+            }
+
+        }
+
+        private void _ViewerKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                gViewer.PanButtonPressed = true;
+                UpdateCursor(System.Windows.Forms.Cursors.Hand);
+            }
+        }
+
+        private void _ViewerKeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+
+            if (!Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                gViewer.PanButtonPressed = false;
+                UpdateCursor(System.Windows.Forms.Cursors.Default);
+            }
+        }
+
+        private void UpdateCursor(System.Windows.Forms.Cursor cursor)
+        {
+            gViewer.Cursor = cursor;
+        }
+
+        private void _ViewerMouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                if (gViewer.SelectedObject.GetType() == typeof(Node) && gViewer.SelectedObject != null)
+                {
+                    var selectedObj = gViewer.SelectedObject;
+                    Node selectedNode = (Node)selectedObj;
+                    string code = huffmanObj.GetCodedSigns()[selectedNode.LabelText];
+                    if (code != null)
+                    {
+                        parentWindow.outputBlock.Text = selectedNode.LabelText + " :: " + code;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+
+
+        }
+    }
+}
